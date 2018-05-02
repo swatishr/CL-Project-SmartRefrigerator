8e0f142e159338fc5ac37a215b3b2593b38a0b87
Jose Ayerdis <necronet@Joses-MacBook-Pro.local>
2018-05-02 13:44:51 -0400

    Add project stem root for lemma

---
+++ project.pl
@@ -0,0 +1,179 @@
+% ===========================================================
+% Main loop:
+% 1. Repeat "input-response" cycle until input starts with "bye"
+%    Each "input-response" cycle consists of:
+% 		1.1 Reading an input string and convert it to a tokenized list
+% 		1.2 Processing tokenized list
+% ===========================================================
+%parse([every,white,container,on,the,bottom,shelf,contains,a,banana],X).
+chat:-
+ repeat,
+   readinput(Input),
+   process(Input),
+  (Input = [bye| _] ),!.
+
+
+% ===========================================================
+% Read input:
+% 1. Read char string from keyboard.
+% 2. Convert char string to atom char list.
+% 3. Convert char list to lower case.
+% 4. Tokenize (based on spaces).
+% ===========================================================
+
+readinput(TokenList):-
+   read_line_to_codes(user_input,InputString),
+   string_to_atom(InputString,CharList),
+   string_lower(CharList,LoweredCharList),
+   tokenize_atom(LoweredCharList,TokenList).
+
+
+% ===========================================================
+%  Process tokenized input
+% 1. Parse morphology and syntax, to obtain semantic representation
+% 2. Evaluate input in the model
+% If input starts with "bye" terminate.
+% ===========================================================
+
+process(Input):-
+	parse(Input,SemanticRepresentation),
+	modelchecker(SemanticRepresentation,Evaluation),
+	respond(Evaluation),!,
+	nl,nl.
+
+process([bye|_]):-
+   write('> bye!').
+
+
+% ===========================================================
+%  Parse:
+% 1. Morphologically parse each token and tag it.
+% 2. Add semantic representation to each tagged token
+% 3. Obtain FOL representation for input sentence
+% ===========================================================
+
+parse(Input, SemanticRepresentation):- write(Input).
+% ...
+
+
+% ===========================================================
+% Grammar
+% 1. List of lemmas
+% 2. Lexical items
+% 3. Phrasal rules
+% ===========================================================
+
+%Every white container on the bottom shelf [tv inf contains [dt a [n banana]]]
+
+
+% --------------------------------------------------------------------
+% Lemmas are uninflected, except for irregular inflection
+% lemma(+Lemma,+Category)
+% --------------------------------------------------------------------
+
+lemma(a,dtexists).
+lemma(an,dtexists).
+lemma(each,dtforall).
+lemma(all,dtforall).
+lemma(every,dtforall).
+lemma(box,n).
+lemma(banana,n).
+lemma(tom,pn).
+lemma(mia,pn).
+lemma(red,adj).
+
+lemma(is,be).
+lemma(was,be).
+
+lemma(eat,tv).
+lemma(ate,tv).
+lemma(drink,tv).
+lemma(drank,tv).
+lemma(drunk,tv).
+lemma(drinks,tv).
+lemma(contain,tv).
+
+lemma(in,p).
+lemma(under,p).
+lemma(on,vacp).
+lemma(to,vacp).
+
+lemma(Word,Tag,Stem) :- atom_chars(Word, WordList),
+                   lemma(Stem,Tag),
+                   atom_chars(Stem, LemmaList),
+                   compare_lemma(WordList,LemmaList),!.
+
+compare_lemma(_,[]).
+compare_lemma([W|WordList],[W|LemmaList]) :-
+                  compare_lemma(WordList,LemmaList).
+% --------------------------------------------------------------------
+% Constructing lexical items:
+% word = lemma + suffix (for "suffix" of size 0 or bigger)
+% --------------------------------------------------------------------
+
+
+lex(n(X^P),Lemma):-
+	lemma(Lemma,n,Stem),
+	P=.. [Stem,X].
+
+lex(dt((X^P)^(X^Q)^forall(X,imp(P,Q))),Word):-
+		lemma(Word,dtforall).
+
+% ...
+
+% --------------------------------------------------------------------
+% Suffix types
+% --------------------------------------------------------------------
+
+% ...
+
+% --------------------------------------------------------------------
+% Phrasal rules
+% rule(+LHS,+ListOfRHS)
+% --------------------------------------------------------------------
+
+rule(np(Y),[dt(X^Y),n(X)]).
+rule(np(X),[pn(X)]).
+
+% ...
+
+
+% ===========================================================
+%  Modelchecker:
+%  1. If input is a declarative, check if true
+%  2. If input is a yes-no question, check if true
+%  3. If input is a content question, find answer
+% ===========================================================
+
+% model(...,...)
+
+% ===========================================================
+%  Respond
+%  For each input type, react appropriately.
+% ===========================================================
+
+% Declarative true in the model
+respond(Evaluation) :-
+		Evaluation = [true_in_the_model],
+		write('That is correct'),!.
+
+% Declarative false in the model
+respond(Evaluation) :-
+		Evaluation = [not_true_in_the_model],
+		write('That is not correct'),!.
+
+% Yes-No interrogative true in the model
+respond(Evaluation) :-
+		Evaluation = [yes_to_question],
+		write('yes').
+
+% Yes-No interrogative false in the model
+respond(Evaluation) :-
+		Evaluation = [no_to_question],
+		write('no').
+
+% wh-interrogative true in the model
+% ...
+
+% wh-interrogative false in the model
+% ...
